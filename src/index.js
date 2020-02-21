cdc07f9ea10c927488bf7027a5c3cec2c419b6ea
Yana Kornejkova <bloom.hog@gmail.com>
2020-02-21 19:10:20 +0300

    feat: add typical-arrays-problems solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (!Array.isArray(array)) {return 0}
+  if (array.length <= 0) {return 0}
+  return Math.min.apply(null, array)
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (!Array.isArray(array)) {return 0}
+  if (array.length <= 0) {return 0}
+  return Math.max.apply(null, array)
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!Array.isArray(array)) {return 0}
+  if (array.length <= 0) {return 0}
+  let avg = 0
+  for (i = 0; i < array.length; i++) {
+    avg = avg +array[i]
+  }
+  return (avg/array.length)
+  
 }
